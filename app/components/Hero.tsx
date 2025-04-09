1bb3c13f469b89dae8341fa2f2d0a128398a4341
Cristobal Andrés Ramirez González <cramirezg@utem.cl>
2025-04-09 16:15:14 -0400

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -101,7 +101,7 @@ export default function Hero({ copyToClipboard, copiedField }: HeroProps) {
           ))}
           <div>
           <a
-            href="https://drive.google.com/file/d/1LAEvPI_ED3JBA3VL8r8iJrlJU4U437Cn/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bWvamT27ZTecVVblK3AB1Usf6fOIJokL/view"
             download
             className="text-primary border-2 border-primary px-4 py-2 rounded-lg hover:bg-primary hover:text-white transition-colors duration-300"
             aria-label="Descargar Curriculum Vitae"
